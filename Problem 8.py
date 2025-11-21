22ce41524e0b2956c2c16159cb738383e77a98bf
nafi_c <132846401+nafic01@users.noreply.github.com>
2025-11-21 19:22:00 +0000

    Problem 8

---
+++ Problem 8.py
@@ -0,0 +1,9 @@
+# Problem 8
+num = 0
+for a in range(1, 1000):
+    for b in range(1, 1000):
+        for c in range(1, 1000):
+            if a + b + c == 1000 and a**2 + b**2 == c**2:
+                num = a*b*c
+print(num)
+
